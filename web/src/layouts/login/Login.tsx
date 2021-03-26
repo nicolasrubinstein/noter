af9bc7461889bec1469faaa78b9a55d58650229b
NicoRubinstein <nicorubinstein1@gmail.com>
2021-03-26 11:32:42 -0300

    remove-warnings

--- web/src/layouts/login/Login.tsx
+++ web/src/layouts/login/Login.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import GoogleLogin from "react-google-login";
 import useLoggedIn from "../../context/LoggedIn";
-import "./Login.scss";
+import styled from "styled-components";
 
 const Login = () => {
   const { setUserInfo, setIsLoggedIn }: any = useLoggedIn();
@@ -13,18 +13,18 @@ const Login = () => {
   };
 
   return (
-    <div className="container">
+    <Container>
       <h1>Your notes, anywhere</h1>
-      <div className="btn-container">
+      <ButtonContainer>
         <GoogleLogin
           clientId="150608648503-eba16gnv5g8s78f60s2n28ia6j7g2jkk.apps.googleusercontent.com"
           buttonText="Login"
           onSuccess={responseGoogle}
           render={(renderProps) => (
-            <button onClick={renderProps.onClick} className="login-button">
+            <LoginButton onClick={renderProps.onClick}>
               <img src="assets/google.png" alt="google icon" width={20} />
               Log in with Google
-            </button>
+            </LoginButton>
           )}
           onFailure={(response) => {
             if (response.googleId) {
@@ -35,9 +35,38 @@ const Login = () => {
           isSignedIn={true}
           className="login-button"
         />
-      </div>
-    </div>
+      </ButtonContainer>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  text-align: center;
+  height: 500px;
+  margin-top: 50px;
+`;
+
+const LoginButton = styled.button`
+  outline: none;
+  margin-top: 50px;
+  background: white;
+  border: none;
+  width: 20vw;
+  height: 50px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  cursor: pointer;
+
+  @media (max-width: 874px) {
+    width: 50vw;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
 
 export default Login;
